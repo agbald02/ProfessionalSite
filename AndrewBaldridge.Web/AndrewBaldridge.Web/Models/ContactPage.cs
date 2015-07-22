8105c75be73e4e6802f44ec97d9f6937864a4289
agbald02 <agbald02@gmail.com>
2015-07-22 17:34:53 -0400

    Changed phone num format

--- AndrewBaldridge.Web/AndrewBaldridge.Web/Models/ContactPage.cs
+++ AndrewBaldridge.Web/AndrewBaldridge.Web/Models/ContactPage.cs
@@ -15,7 +15,7 @@ namespace AndrewBaldridge.Web.Models
             ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Please enter a phone number")]
-        [RegularExpression(@"^[01]?[- .]?(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?\d{3}[- .]?\d{4}$", ErrorMessage = "Please enter a valid phone number.")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Please enter a valid phone number.")]
         public string Phone { get; set; }
         [Required(ErrorMessage = "Please enter a message")]
         public string Message { get; set; }
