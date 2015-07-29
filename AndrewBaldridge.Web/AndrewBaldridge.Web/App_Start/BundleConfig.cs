c6462bf10a095d278ebf419b7c53e9d03ca64760
agbald02 <agbald02@gmail.com>
2015-07-29 16:13:24 -0400

    Bundling and Google Analyics

---
+++ AndrewBaldridge.Web/AndrewBaldridge.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Optimization;
+
+namespace AndrewBaldridge.Web.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+#if !DEBUG
+            bundles.UseCdn = true;
+            BundleTable.EnableOptimizations = true;
+#endif
+            bundles.Add(new StyleBundle("~/bundles/commoncss")
+                .Include("~/Content/Styles.css")
+                .Include("~/Content/bootstrap.css")
+                .Include("~/Content/bootstrap-theme-css")
+                );
+
+            bundles.Add(new StyleBundle("~/bundles/bootstrapcss",
+                "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
+                .Include("~/Content/bootstrap.css")
+                );
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapjs",
+                "//maxcdn.bootstrapcdn.comm/bootstrap/3.3.1/js/bootstrap.min/js")
+                .Include("~/Scripts/bootstrap.js")
+                );
+
+            bundles.Add(new ScriptBundle("~/bundles/commonjavascript")
+            .Include("~/Scripts/bootstrap.js")
+            .Include("~/Scripts/jquery-2.1.4.js")
+            .Include("~/Scripts/unslider.min.js.js")
+            );
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
--- AndrewBaldridge.Web/AndrewBaldridge.Web/Global.asax.cs
+++ AndrewBaldridge.Web/AndrewBaldridge.Web/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using AndrewBaldridge.Web.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace AndrewBaldridge.Web
@@ -11,9 +13,11 @@ namespace AndrewBaldridge.Web
     {
         protected void Application_Start()
         { 
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             AreaRegistration.RegisterAllAreas();
+
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
